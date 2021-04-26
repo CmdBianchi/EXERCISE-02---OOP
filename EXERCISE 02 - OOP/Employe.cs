f2e426cfb96cec337f7a5401156f4ac746822ac4
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-26 09:49:13 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 02 - OOP/Employe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+namespace EXERCISE_02___OOP{
+    //---------------------- START | OBJ EMPLOYE ------------------------//
+    class Employe{ //----> CLASS ATTRIBUTES
+        public string Name;
+        public double GroossSalary;
+        public double Tax;
+        //------------------------ MET. CLASS -------------------------------//
+        public double NetSalary(){
+            return GroossSalary - Tax;
+        }
+        public void IncreaseSalary(double percentage){
+            GroossSalary = GroossSalary + (GroossSalary * percentage / 100);
+        }
+        public override string ToString(){
+            return Name
+            + ", R$ "
+            + NetSalary().ToString("F2");
+        }
+    }
+    //--------------------------- END --------------------------------//
+}
---
+++ EXERCISE 02 - OOP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EXERCISE_02___OOP
+{
+    class Program{
+        //-----> INICIO
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        static void Main(string[] args){
+            Employe emp = new Employe();
+
+            Console.Write("Nome do funcionario: "); emp.Name = Console.ReadLine();
+            Console.Write("Salario Bruto: "); emp.GroossSalary = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: "); emp.Tax = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Funcionario: "+emp);
+            Console.WriteLine();
+            Console.Write("Deseja aumentar o salário? "); double percentage = double.Parse(Console.ReadLine());
+            emp.IncreaseSalary(percentage); //-----> SEND INTEL TO PERCENTAGE CLASS EMPLOYE
+            Console.WriteLine();
+            Console.WriteLine("DADOS ATUAIS: " +emp);          
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        //-----> FIM
+    }
+}
